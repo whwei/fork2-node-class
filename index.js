d97965549886f2c503dd40b7bd52dcdb480b0628
whwei <whw.0922@gmail.com>
2014-06-26 14:41:00 +0800

    complete version

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var Class = function(o) {
 };
 
 Class.create = function(prop, parent) {
+    var value,
+        proto,
+        init;
+
     prop = prop || {};
     parent = parent || Object;
+
+    init = prop.initialize;
 
     function f() {
         parent.apply(this, arguments);
 
-        if (this.constructor === f && this.initialize) {
-            this.initialize.apply(this, arguments);
+        if (this.constructor === f && init) {
+            init.apply(this, arguments);
         }
     }
 
@@ -26,16 +32,55 @@ Class.create = function(prop, parent) {
     }
 
     ctor.prototype = parent.prototype;
+    proto = ctor.prototype;
 
     var c  = new ctor();
 
     for (var k in prop) {
-        c[k] = prop[k];
+        if (k !== 'initialize') {
+            value = prop[k];
+            c[k] = value;
+            // c[k] = isFn(value) && isFn(proto[k]) ? 
+            //     (function(name, subFn) {
+
+            //         return function() {
+            //             var ret,
+            //                 tmp;
+
+            //             tmp = this.super;
+
+            //             // this.super('method', ...) => this.super(...) 
+            //             this.super = function() {
+            //                 var args = [].slice.call(arguments, 1);
+            //                 return ctor.prototype[name].apply(this, args);
+            //             };
+                        
+            //             ret = subFn.apply(this, arguments);
+
+            //             this.super = tmp;
+
+            //             return ret;
+            //         };
+            //     })(k, value)
+            //     : value;
+        };
+        
     }
 
-    c.super = function () {
-        // argument.callee.caller
-    }
+    c.super = function (method) {
+        var ret,
+            tmp = this.super;
+
+        this.super = function () {
+          return proto.__proto__[method].apply(this, [].slice.call(arguments, 1));
+        };
+
+        ret = proto[method].apply(this, [].slice.call(arguments, 1));
+
+        this.super = tmp;
+
+        return ret;
+    };
 
     c.constructor = f;
 
@@ -45,5 +90,10 @@ Class.create = function(prop, parent) {
 
     return f;
 };
+
+// helper
+function isFn(v) {
+    return Object.prototype.toString.call(v) === '[object Function]';
+}
 
 module.exports = Class.create;
